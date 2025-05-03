8cc14a4828105c4e0ec00534600d16b918ddc1eb
vikashraosahab <vikashvinayrao@gmail.com>
2025-05-03 11:15:17 +0530

    initial commit

---
+++ src/main.js
@@ -0,0 +1,8 @@
+// IMPORT OTHER MODULES 
+import {UI} from './ui.js';
+
+
+// CONNECTION TO THE INDEX HTML 
+const root = document.getElementById('app');
+
+root.innerHTML = 'Hello World!';
---
+++ src/ui.js
@@ -0,0 +1,8 @@
+export const ui = `<div class= "container">
+    <header>
+       <div class ='logo'>
+         <img src = "https://cdn-icons-png.flaticon.com/512/1214/1214438.png" alt = "logo" />
+         <h1> Todo List</h1>
+       </div>
+    </header>
+</div>`
